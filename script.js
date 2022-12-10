6e0a401c43022104b0bd94d32fb804a3b616750c
TAYFUN FIRTINA <39940749+TayfunF@users.noreply.github.com>
2022-12-10 21:12:42 +0300

    files updated

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 let userFormDOM = document.querySelector("#userForm");
 userFormDOM.addEventListener("submit", formHeandler);
 
+const showAlert = (message, classname = "warning") => `
+<div class="alert alert-${classname} d-flex align-items-center" role="alert">
+  <div>${message}</div>
+</div>
+`;
+const myAlertDOM = document.querySelector("#myAlert");
+
 function formHeandler(event) {
   event.preventDefault();
   let USERNAME = document.querySelector("#username");
   let SCORE = document.querySelector("#score");
-  addItem(USERNAME.value, SCORE.value);
+  if (USERNAME.value && SCORE.value) {
+    addItem(USERNAME.value, SCORE.value);
+    USERNAME = "";
+    SCORE = "";
+  } else {
+    myAlertDOM.innerHTML = showAlert(
+      "Kullanıcı adı ya da şifre boş olamaz",
+      "danger"
+    );
+  }
 }
 
 let userListDOM = document.querySelector("#userList");
